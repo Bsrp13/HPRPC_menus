2540f10d71438a2e80296f57d457129f21e0318d
Bsrp13 <139605782+Bsrp13@users.noreply.github.com>
2023-08-01 18:52:12 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'common'
+
+description 'HPRPC - Menus'
+version '1.0'
+author 'Ky.H [Ky.H#6521]'
+description 'This resource was built for HPRPC and provides menu functions for all HPRPC resources built by Ky.H.'
+
+client_scripts {
+	'menus.lua'
+}
+
+lua54 'yes'
